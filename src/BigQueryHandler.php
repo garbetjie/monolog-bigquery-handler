5b81ea12f908d51f357d92f66e8a570f8964af29
Geoff Garbers <geoff@garbers.co.za>
2018-08-25 20:59:16 +0200

    Add ability to set custom fields. Fixed #1.

--- src/BigQueryHandler.php
+++ src/BigQueryHandler.php
@@ -3,16 +3,42 @@
 namespace Garbetjie\Monolog\Handler;
 
 use Google\Cloud\BigQuery\BigQueryClient;
+use Google\Cloud\BigQuery\Table;
 use Monolog\Handler\AbstractProcessingHandler;
 use Monolog\Logger;
 
 class BigQueryHandler extends AbstractProcessingHandler
 {
+    /**
+     * @var BigQueryClient
+     */
     private $client;
+
+    /**
+     * @var Table
+     */
     private $table;
+
+    /**
+     * @var null|bool
+     */
     private $handling = null;
 
-    public function __construct(BigQueryClient $bigQueryClient, $dataSetName, $tableName, $level = Logger::DEBUG, $bubble = true)
+    /**
+     * @var array
+     */
+    private $customFields = [];
+
+    /**
+     * BigQueryHandler constructor.
+     *
+     * @param BigQueryClient $bigQueryClient
+     * @param string $dataSetName
+     * @param string $tableName
+     * @param int $level
+     * @param bool $bubble
+     */
+    public function __construct(BigQueryClient $bigQueryClient, string $dataSetName, string $tableName, $level = Logger::DEBUG, $bubble = true)
     {
         parent::__construct($level, $bubble);
 
@@ -20,7 +46,45 @@ class BigQueryHandler extends AbstractProcessingHandler
         $this->table = $bigQueryClient->dataset($dataSetName)->table($tableName);
     }
 
-    public function isHandling(array $record)
+    /**
+     * Set a custom field (or multiple) to be inserted along with the Monolog record.
+     *
+     * An array of custom fields can be added, or a single $name/$value pair. Any custom fields that are the same as
+     * the core fields will be ignored.
+     *
+     * @param string|array $name
+     * @param mixed [$value]
+     */
+    public function setCustomField($name, $value = null)
+    {
+        if (\is_array($name)) {
+            $this->customFields = \array_merge($this->customFields, $name);
+        } else {
+            $this->customFields[$name] = $value;
+        }
+    }
+
+    /**
+     * Remove a custom field that has been previously added.
+     *
+     * @param string|array $name
+     */
+    public function removeCustomField($name)
+    {
+        if (! \is_array($name)) {
+            $name = [$name];
+        }
+
+        foreach ($name as $key) {
+            unset($this->customFields[$key]);
+        }
+    }
+
+    /**
+     * @param array $record
+     * @return bool
+     */
+    public function isHandling(array $record): bool
     {
         // Levels don't match, so we're not handling this record.
         if (!parent::isHandling($record)) {
@@ -38,16 +102,22 @@ class BigQueryHandler extends AbstractProcessingHandler
         return $this->handling;
     }
 
+    /**
+     * @inheritdoc
+     */
     protected function write(array $record)
     {
         $this->handleBatch([$record]);
     }
 
+    /**
+     * @inheritdoc
+     */
     public function handleBatch(array $records)
     {
         $records = \array_map(
             function(array $record) {
-                $record['extra'] = $this->prepareRecordValues($record['extra']);
+                $customFields = $this->prepareRecordValues($this->customFields);
 
                 return [
                     'data' => [
@@ -58,7 +128,7 @@ class BigQueryHandler extends AbstractProcessingHandler
                         'context' => \json_encode($this->prepareRecordValues($record['context']) ?: new \stdClass()),
                         'extra' => \json_encode($this->prepareRecordValues($record['extra']) ?: new \stdClass()),
                         'logged_at' => $record['datetime']->format('Y-m-d H:i:s.uP')
-                    ]
+                    ] + $customFields
                 ];
             },
             $records
